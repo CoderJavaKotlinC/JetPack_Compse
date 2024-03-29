6bfe4ee97807d5d065c0fbbf38cad7b2ede2c6d5
CoderJavaKotlinC <162939511+CoderJavaKotlinC@users.noreply.github.com>
2024-03-28 18:55:38 -0600

    Update TextEdit&Borders.kts

--- TextEdit&Borders.kts
+++ TextEdit&Borders.kts
@@ -1,8 +1,8 @@
-    override fun onCreate(savedInstanceState: Bundle?) {
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // Enter each layout function here to see their UI effect
-            Title("Hello", "World")
+            TitleTwo("Hello", "World")
 
         }
     }
@@ -28,7 +28,7 @@
 
 
       ) {
-          //Example of how you edit text. see how it appears on the ui 
+          //Example of how you edit text. see how it appears on the ui
           Text(text = title1, modifier = Modifier.border(5.dp, androidx.compose.ui.graphics.Color.Yellow)
               .padding(5.dp)
               .offset(20.dp)
@@ -41,3 +41,35 @@
 
 
   }
+    @Composable
+    fun TitleTwo(title1: String, title2: String){
+        Column(
+            modifier = Modifier
+                .background(androidx.compose.ui.graphics.Color.Green)
+                .fillMaxHeight(0.5f) // fills max width or covers the percentage of the UI
+                .fillMaxWidth() // fills max width or up to certain percent
+                .width(300.dp) // specifies space needed
+                // .requiredWidth(600.dp) // sets the total required space.
+                .padding(top = 50.dp)
+                .border(5.dp, androidx.compose.ui.graphics.Color.Magenta) // adding borders
+                // adding layers of boarders
+                .padding(5.dp)
+                .border(5.dp, androidx.compose.ui.graphics.Color.Blue)
+                .padding(5.dp)
+                .border(10.dp, androidx.compose.ui.graphics.Color.Red)
+                .padding(10.dp)
+
+
+        ) {
+            //Example of how you edit text. see how it appears on the ui
+            Text(text = title1, modifier = Modifier.clickable { // Makes text clickable
+                // Here is where you enter the clickable action
+
+            })
+
+            Spacer(modifier = Modifier.height(50.dp)) // spaces the text from each other
+            Text(text = title2)
+        }
+
+
+    }
